f89b7e5295599d9af976e1937c82d68a969360bc
huqianqian <huqianqian_giser@126.com>
2025-04-08 17:56:47 +0900

    0408

---
+++ MapBox/perg-master/perg-master/test_src/openmp_demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main() {
+    // Set the number of threads to 4
+
+    int nthreads, tid;
+
+    // Set the number of threads to 4
+    omp_set_num_threads(4);
+    // Parallel region
+    // Each thread will execute the code inside the parallel region
+    #pragma omp parallel private(nthreads, tid)
+    {
+        tid = omp_get_thread_num();
+        cout << "Hello World from thread " << tid << endl;
+
+        if (tid == 0) {
+            nthreads = omp_get_num_threads();
+            cout << "Number of threads = " << nthreads << endl;
+        }
+    } // implicit barrier here
+
+    return 0;
+}
