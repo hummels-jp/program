44a7b78fc2c01a6f60550e401ce04d78af62018f
huqianqian <huqianqian_giser@126.com>
2025-04-17 17:10:02 +0900

    0417

--- MapBox/perg-master/fileSearch0413/include/ThreadPool.h
+++ MapBox/perg-master/fileSearch0413/include/ThreadPool.h
@@ -13,17 +13,15 @@
 class ThreadPool {
 public:
     // Get the singleton instance of ThreadPool
-    static ThreadPool& getInstance(size_t numThreads = std::thread::hardware_concurrency()) {
-        static ThreadPool instance(numThreads);
-        return instance;
-    }
+    static ThreadPool& getInstance(size_t numThreads = std::thread::hardware_concurrency());
 
     // Disable copy and assignment
     ThreadPool(const ThreadPool&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
 
     template <typename F>
-    auto enqueue(F&& f) -> std::future<decltype(f())> {
+    auto enqueue(F&& f) -> std::future<decltype(f())>
+    {
         auto task = std::make_shared<std::packaged_task<decltype(f())()>>(std::forward<F>(f));
         std::future<decltype(f())> result = task->get_future();
         {
@@ -37,44 +35,12 @@ public:
         return result;
     }
 
-    size_t getFreeThreadCount() const {
-        return free_thread_count.load();
-    }
+    size_t getFreeThreadCount() const;
 
 private:
     // Private constructor
-    ThreadPool(size_t numThreads) : stop(false), free_thread_count(0) {
-        for (size_t i = 0; i < numThreads; ++i) {
-            workers.emplace_back([this]() {
-                while (true) {
-                    std::packaged_task<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(queueMutex);
-                        condition.wait(lock, [this]() { return stop.load() || !tasks.empty(); });
-                        if (stop.load() && tasks.empty()) return;
-
-                        task = std::move(tasks.front());
-                        tasks.pop();
-                        --free_thread_count; // Decrease idle thread count
-                    }
-                    task();
-                    ++free_thread_count; // Increase idle thread count
-                }
-            });
-            ++free_thread_count; // All threads are idle at initialization
-        }
-    }
-
-    ~ThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-            stop.store(true); // Set atomic variable to true
-        }
-        condition.notify_all();
-        for (std::thread &worker : workers) {
-            worker.join();
-        }
-    }
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
 
     std::vector<std::thread> workers; // Worker threads
     std::queue<std::packaged_task<void()>> tasks; // Task queue
---
+++ MapBox/perg-master/fileSearch0413/src/ThreadPool.cpp
@@ -0,0 +1,48 @@
+#include "ThreadPool.h"
+#include <stdexcept>
+
+// Get the singleton instance of ThreadPool
+ThreadPool& ThreadPool::getInstance(size_t numThreads) {
+    static ThreadPool instance(numThreads);
+    return instance;
+}
+
+// Private constructor
+ThreadPool::ThreadPool(size_t numThreads) : stop(false), free_thread_count(0) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers.emplace_back([this]() {
+            while (true) {
+                std::packaged_task<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(queueMutex);
+                    condition.wait(lock, [this]() { return stop.load() || !tasks.empty(); });
+                    if (stop.load() && tasks.empty()) return;
+
+                    task = std::move(tasks.front());
+                    tasks.pop();
+                    --free_thread_count; // Decrease idle thread count
+                }
+                task();
+                ++free_thread_count; // Increase idle thread count
+            }
+        });
+        ++free_thread_count; // All threads are idle at initialization
+    }
+}
+
+// Destructor
+ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        stop.store(true); // Set atomic variable to true
+    }
+    condition.notify_all();
+    for (std::thread& worker : workers) {
+        worker.join();
+    }
+}
+
+// Get the count of free threads
+size_t ThreadPool::getFreeThreadCount() const {
+    return free_thread_count.load();
+}
