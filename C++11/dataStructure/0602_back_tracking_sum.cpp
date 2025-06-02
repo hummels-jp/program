ad33e219af050e2b6721ad3d3791a0db33635864
huqianqian <huqianqian_giser@126.com>
2025-06-02 11:51:33 +0900

    0602

--- C++11/dataStructure/0602_back_tracking_sum.cpp
+++ C++11/dataStructure/0602_back_tracking_sum.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int arr[] ={2,4,9,11,12,13};
 const int len = sizeof(arr)/sizeof(arr[0]);
-int target = 18;
+int target = 24; // 目标值
 vector<int> x; // 辅助数组，用来记录被选择的数
 
 void helper(int i, int target)
 {
     // 找到符合条件的子数组
-    if (target == 0)
+    if (0 == target)
     {
         /* code */
         for (int value : x)
@@ -29,12 +29,14 @@ void helper(int i, int target)
         for (int j =i; j < len; j++)
         {
             // 如果剩下的元素小于需要构建的数字，向下递归
-            if (target > arr[j])
+            if (target>= arr[j])
             {
                 /* code */
                 x.push_back(arr[j]);
-                helper(j+1, target - arr[j]); // 不允许重复选择元素
+                // helper(j+1, target - arr[j]); // 不允许重复选择元素
+                helper(j, target - arr[j]); // 允许重复选择元素
                 x.pop_back();
+
             }
         }  
     }
